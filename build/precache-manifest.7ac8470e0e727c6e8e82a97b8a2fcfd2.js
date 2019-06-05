88df26798e7ab5abfabd6fc7dce7854913cd509b
IvanDen <denisko.iv90@gmail.com>
2019-06-05 18:15:04 +0400

    commit builds

---
+++ build/precache-manifest.7ac8470e0e727c6e8e82a97b8a2fcfd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ae5db409edec4501f499f31f8b0908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "323f0a96465dfb53febd",
+    "url": "/static/css/main.e0ddcdc3.chunk.css"
+  },
+  {
+    "revision": "f5c69ff8ba2e55ee5738",
+    "url": "/static/js/2.e0fb6ec7.chunk.js"
+  },
+  {
+    "revision": "323f0a96465dfb53febd",
+    "url": "/static/js/main.d6ae35d3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
